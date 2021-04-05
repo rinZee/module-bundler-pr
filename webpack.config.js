b1c7e4cc28b48eef65ca1f9aec86c39fa1c8d564
unknown <trex33160@gmail.com>
2021-04-05 15:39:14 +0545

    tested scss files in webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+module.exports = {
+entry: './src/index.js',
+output: {
+    filename: 'awesome.js',
+    path: path.resolve(__dirname, 'dist'),
+},
+module: {
+    rules: [
+        {
+        test: /\.scss$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+        ],
+    },
+    ],
+},
+
+devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: 9000
+}
+};
